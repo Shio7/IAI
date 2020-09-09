6e1bf4c60ef8f13d02c81d8f9c4ed2a3aacabbc7
Shio <shio7113@gmail.com>
2020-09-09 13:11:51 +0900

    asdfasdfasdfasd

---
+++ resize.py
@@ -0,0 +1,18 @@
+# resize pokeGAN.py
+import os
+import cv2
+from os.path import isdir
+
+src = "./data" #pokeRGB_black
+dst = "./resizedData" # resized
+
+os.mkdir(dst)
+
+i=0
+for each in os.listdir(src):
+    i+=1
+    print(str(i)+ " worked")
+    img = cv2.imread(os.path.join(src,each))
+    img = cv2.resize(img,(256,144))
+    cv2.imwrite(os.path.join(dst,each), img)
+    
